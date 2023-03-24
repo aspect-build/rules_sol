0a8a1a0565d04b3f9d746b84b315da965bf8075f
Alex Eagle <alex@aspect.dev>
2023-03-24 12:13:46 -0700

    Update Older.sol

--- examples/compiler_selection/Older.sol
+++ examples/compiler_selection/Older.sol
@@ -5,7 +5,6 @@ pragma solidity >=0.7.6 <0.7.7;
 
 // Underflow is illegal starting in Solidity 0.8.0
 // https://blog.soliditylang.org/2020/12/16/solidity-v0.8.0-release-announcement/
-// Therefore this code would fail to compile unless the `solc_version` attribute is honored.
 contract C {
     function f() public pure {
         uint x = 0;
